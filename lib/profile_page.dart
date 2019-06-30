09241f408e96e7c6b39b87ca40889756382c870f
Kong Huey Yee <erinkong84@gmail.com>
2019-07-01 01:25:27 +0800

    first commit

---
+++ lib/profile_page.dart
@@ -0,0 +1,305 @@
+import 'package:flutter_web/material.dart';
+import 'package:flutter_web_portfolio/responsive_widget.dart';
+
+class ProfilePage extends StatelessWidget {
+  const ProfilePage({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return ResponsiveWidget(
+      largeScreen: Scaffold(
+        backgroundColor: Colors.black,
+        appBar: AppBar(
+          elevation: 0.0,
+          backgroundColor: Colors.black,
+        ),
+        drawer: ResponsiveWidget.isSmallScreen(context)
+            ? Drawer(
+                child: ListView(
+                  children: <Widget>[],
+                ),
+              )
+            : null,
+        body: SingleChildScrollView(
+          child: AnimatedPadding(
+            duration: Duration(seconds: 1),
+            padding: EdgeInsets.all(MediaQuery.of(context).size.height * 0.1),
+            child: ResponsiveWidget(
+              largeScreen: Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: <Widget>[
+                  NavHeader(),
+                  SizedBox(
+                    height: MediaQuery.of(context).size.height * 0.1,
+                  ),
+                  ProfileInfo(),
+                  SizedBox(
+                    height: MediaQuery.of(context).size.height * 0.1,
+                  ),
+                  SocialInfo(),
+                ],
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class NavHeader extends StatelessWidget {
+  Widget build(BuildContext context) {
+    return ResponsiveWidget(
+      largeScreen: Row(
+        mainAxisAlignment: ResponsiveWidget.isSmallScreen(context)
+            ? MainAxisAlignment.center
+            : MainAxisAlignment.spaceBetween,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: <Widget>[
+          PKDot(),
+//          Spacer(),
+          if (!ResponsiveWidget.isSmallScreen(context))
+            Row(
+              children: <Widget>[
+                NavButton(
+                  text: "about",
+                  onPressed: () {},
+                ),
+                NavButton(
+                  text: "work",
+                  onPressed: () {},
+                ),
+                NavButton(
+                  text: "contact",
+                  onPressed: () {},
+                ),
+              ],
+            )
+        ],
+      ),
+    );
+  }
+}
+
+class PKDot extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: <Widget>[
+        Text(
+          "UI UX DEVELOPER",
+          textScaleFactor: 2,
+          style: TextStyle(
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        SizedBox(
+          width: 5,
+        ),
+        AnimatedContainer(
+          duration: Duration(seconds: 1),
+          height: 8,
+          width: 8,
+          decoration: BoxDecoration(
+            shape: BoxShape.circle,
+            color: Colors.orange,
+          ),
+        ),
+      ],
+    );
+  }
+}
+
+class NavButton extends StatelessWidget {
+  final text;
+  final onPressed;
+  final Color color;
+
+  const NavButton(
+      {Key key,
+      @required this.text,
+      @required this.onPressed,
+      this.color = Colors.orange})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return OutlineButton(
+      child: Text(text),
+      borderSide: BorderSide(
+        color: color,
+      ),
+      onPressed: onPressed,
+      highlightedBorderColor: color,
+    );
+  }
+}
+
+class ProfileInfo extends StatelessWidget {
+  profileImage(context) => Container(
+        height: ResponsiveWidget.isSmallScreen(context)
+            ? MediaQuery.of(context).size.height * 0.25
+            : MediaQuery.of(context).size.width * 0.25,
+        width: ResponsiveWidget.isSmallScreen(context)
+            ? MediaQuery.of(context).size.height * 0.25
+            : MediaQuery.of(context).size.width * 0.25,
+        decoration: BoxDecoration(
+          backgroundBlendMode: BlendMode.luminosity,
+          color: Colors.deepPurple,
+//            borderRadius: BorderRadius.circular(40),
+          shape: BoxShape.circle,
+          image: DecorationImage(
+            image: AssetImage("ek.jpg"),
+            alignment: Alignment.center,
+            fit: BoxFit.cover,
+          ),
+        ),
+      );
+
+  final profileData = Column(
+    crossAxisAlignment: CrossAxisAlignment.start,
+    children: <Widget>[
+      Text(
+        "Hi there! My name is",
+        textScaleFactor: 2,
+        style: TextStyle(color: Colors.pink),
+      ),
+      Text(
+        "Erin\nKong",
+        textScaleFactor: 5,
+        style: TextStyle(
+          color: Colors.white,
+          fontWeight: FontWeight.bold,
+        ),
+      ),
+      SizedBox(
+        height: 10,
+      ),
+      Text(
+        "I am Erin.\n"
+        "Be Humble ,Be Passionate and Determind Learning Everywhere \n"
+        "An Flutter Girl who is curious about UI design and User Experience.",
+        softWrap: true,
+        textScaleFactor: 1.5,
+        style: TextStyle(color: Colors.white70),
+      ),
+      SizedBox(
+        height: 20,
+      ),
+      Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: <Widget>[
+          RaisedButton(
+            shape: StadiumBorder(),
+            child: Text("welcome"),
+            color: Colors.red,
+            onPressed: () {},
+            padding: EdgeInsets.all(10),
+          ),
+          SizedBox(
+            width: 20,
+          ),
+          OutlineButton(
+            borderSide: BorderSide(
+              color: Colors.red,
+            ),
+            shape: StadiumBorder(),
+            child: Text("Say Hi!"),
+            color: Colors.red,
+            onPressed: () {},
+            padding: EdgeInsets.all(10),
+          )
+        ],
+      )
+    ],
+  );
+
+  @override
+  Widget build(BuildContext context) {
+    return ResponsiveWidget(
+      largeScreen: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: <Widget>[profileImage(context), profileData],
+      ),
+      smallScreen: Column(
+        mainAxisSize: MainAxisSize.max,
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: <Widget>[
+          profileImage(context),
+          SizedBox(
+            height: MediaQuery.of(context).size.height * 0.1,
+          ),
+          profileData
+        ],
+      ),
+    );
+  }
+}
+
+class SocialInfo extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return ResponsiveWidget(
+      largeScreen: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: <Widget>[
+          Row(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: <Widget>[
+              NavButton(
+                text: "Github",
+                onPressed: () {},
+                color: Colors.blue,
+              ),
+              NavButton(
+                text: "Twitter",
+                onPressed: () {},
+                color: Colors.blue,
+              ),
+              NavButton(
+                text: "Facebook",
+                onPressed: () {},
+                color: Colors.blue,
+              ),
+            ],
+          ),
+          Text(
+            "Erin ©️2019",
+            textAlign: TextAlign.center,
+            style: TextStyle(
+              color: Colors.grey,
+            ),
+          ),
+        ],
+      ),
+      smallScreen: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: <Widget>[
+          NavButton(
+            text: "Github",
+            onPressed: () {},
+            color: Colors.blue,
+          ),
+          NavButton(
+            text: "Twitter",
+            onPressed: () {},
+            color: Colors.blue,
+          ),
+          NavButton(
+            text: "Facebook",
+            onPressed: () {},
+            color: Colors.blue,
+          ),
+          Text(
+            "Erin  ©️2019",
+            textAlign: TextAlign.center,
+            style: TextStyle(
+              color: Colors.grey,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
